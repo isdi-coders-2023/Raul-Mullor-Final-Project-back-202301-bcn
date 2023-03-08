b7a96a1f16e15a58ad79891de09bd7bc7e4ef73a
RaulMullor <mullor92@gmail.com>
2023-03-08 10:42:45 +0100

    add cors options and errors on index.ts

--- src/server/startServer.ts
+++ src/server/startServer.ts
@@ -1,11 +1,24 @@
 import createDebug from "debug";
+import { type CustomError } from "../CustomError/CustomError.js";
 import { app } from "./index.js";
 
 const debug = createDebug("onepath:server:startServer");
 
-const startServer = async (port: number) => {
-  app.listen(port);
-  debug("Hola");
-};
+const startServer = async (port: number) =>
+  new Promise((resolve, reject) => {
+    const server = app.listen(port, () => {
+      resolve(server);
+    });
+
+    server.on("error", (error: CustomError) => {
+      const errorMessage = "Error on starting the server.";
+
+      if (error.code === "EADDRINUSE") {
+        debug(errorMessage, `The port number ${port} is already in use`);
+      }
+
+      reject(new Error(errorMessage));
+    });
+  });
 
 export default startServer;
