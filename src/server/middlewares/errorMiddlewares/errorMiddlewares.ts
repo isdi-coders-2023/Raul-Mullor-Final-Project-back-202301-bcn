af652c43c6c24ab45769ea54a4073a4097638209
RaulMullor <mullor92@gmail.com>
2023-03-08 09:41:06 +0100

    create error Middlewares

---
+++ src/server/middlewares/errorMiddlewares/errorMiddlewares.ts
@@ -0,0 +1,38 @@
+import createDebug from "debug";
+import { type NextFunction, type Request, type Response } from "express";
+import { CustomError } from "../../../CustomError/CustomError";
+
+const debug = createDebug("pathone:server:middlewares:errorMiddlewares");
+const notFoundStatusCode = 404;
+const generalErrorStatusCode = 500;
+const publicMessageText = "Something went wrong";
+
+export const notFoundError = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const error = new CustomError(
+    "Path not found",
+    notFoundStatusCode,
+    "Endpoint not found"
+  );
+
+  next(error);
+};
+
+export const generalError = (
+  error: CustomError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  debug(error.message);
+
+  const statusCode = error.statusCode || generalErrorStatusCode;
+  const publicMessage = error.publicMessage || publicMessageText;
+
+  res.status(statusCode).json({ error: publicMessage });
+};
+
+export { CustomError };
